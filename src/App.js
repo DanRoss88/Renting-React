7512dbec1a9546def90e9ebc7464bf7c137b8e97
DanRoss88 <danielmartinross@gmail.com>
2023-07-20 06:39:36 -0700

    work on connecting a user

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
   return (
     <div className="App">
    
-    <Home />
+    {/* <Home /> */}
+    <Login />
+     <Register /> 
     </div>
   );
 }
---
+++ src/components/PropertyPost.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+
+export default function PostRentalHousing() {
+  const [location, setLocation] = useState({ lat: 0, lng: 0 });
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+  });
+
+  const handleMapClick = (e) => {
+    setLocation({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+  };
+
+  const handlePost = async () => {
+    try {
+      await axios.post("/api/rental-housing", { location });
+      // Handle success or redirect
+    } catch (error) {
+      // Handle error
+    }
+  };
+
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading maps...";
+
+  return (
+    <div>
+      <h2>Post Rental Housing</h2>
+      <GoogleMap
+        mapContainerStyle={{ height: "400px", width: "100%" }}
+        center={location}
+        zoom={10}
+        onClick={handleMapClick}
+      >
+        <Marker position={location} />
+      </GoogleMap>
+      <button onClick={handlePost}>Post</button>
+    </div>
+  );
+};
+
+ 
